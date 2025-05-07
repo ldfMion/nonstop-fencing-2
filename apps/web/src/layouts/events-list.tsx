851d499a6c76e83487b6cbb38afe46d3ab69b0dc
ldfMion <lorenzodfmion@gmail.com>
2025-05-07 14:33:57 -0300

    made more ui adjustments

--- apps/web/src/layouts/events-list.tsx
+++ apps/web/src/layouts/events-list.tsx
@@ -24,15 +24,19 @@ export function EventsList({
 			</div>
 		);
 	}
+	// Sort events by date with the latest first
+	events.sort(
+		(a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+	);
 
 	// Group events by month
 	const groupedEvents = events.reduce(
 		(groups, event) => {
-			const monthYear = format(new Date(event.date), "MMMM yyyy");
-			if (!groups[monthYear]) {
-				groups[monthYear] = [];
+			const month = format(new Date(event.date), "MMMM");
+			if (!groups[month]) {
+				groups[month] = [];
 			}
-			groups[monthYear].push(event);
+			groups[month].push(event);
 			return groups;
 		},
 		{} as Record<string, (Event & { flag: string })[]>
@@ -66,83 +70,71 @@ export function EventsList({
 			: "bg-indigo-100 text-indigo-800 hover:bg-indigo-200";
 	};
 
-	return Object.entries(groupedEvents).map(([month, monthEvents]) => (
-		<div key={month} className="flex flex-col">
-			<h2 className="text-xl font-semibold mb-4 px-4">{month}</h2>
-			<div className="overflow-hidden gap-2 flex flex-col">
-				{monthEvents.map((event, index) => (
-					<Card key={event.id}>
-						<CardContent className="flex flex-col gap-1">
-							<div className="flex flex-col">
-								<div className="flex flex-row gap-2">
-									<div className="flex-shrink-0 w-10 h-7 overflow-hidden rounded-md border">
-										<Image
-											src={`https://flagcdn.com/w1280/${event.flag.toLowerCase()}.png`}
-											alt={`${event.flag} flag`}
-											className="w-full h-full object-cover"
-											height={400}
-											width={400}
-										/>
+	return (
+		<div className="flex flex-col gap-4">
+			{Object.entries(groupedEvents).map(([month, monthEvents]) => (
+				<div key={month} className="flex flex-col gap-2">
+					<h2 className="text-xl font-semibold px-4">{month}</h2>
+					<div className="overflow-hidden gap-2 flex flex-col">
+						{monthEvents.map((event, index) => (
+							<Card key={event.id}>
+								<CardContent className="flex flex-col gap-1">
+									<div className="flex flex-col">
+										<div className="flex flex-row gap-2">
+											<div className="flex-shrink-0 w-10 h-7 overflow-hidden rounded-md border">
+												<Image
+													src={`https://flagcdn.com/w1280/${event.flag.toLowerCase()}.png`}
+													alt={`${event.flag} flag`}
+													className="w-full h-full object-cover"
+													height={400}
+													width={400}
+												/>
+											</div>
+											<CardTitle className="text-lg font-medium text-gray-900">
+												{event.name}
+											</CardTitle>
+										</div>
+										<CardDescription className="text-sm text-gray-500 mt-1">
+											{format(
+												new Date(event.date),
+												"MMM d"
+											)}
+										</CardDescription>
 									</div>
-									<CardTitle className="text-lg font-medium text-gray-900">
-										{event.name}
-									</CardTitle>
-								</div>
-								<CardDescription className="text-sm text-gray-500 mt-1">
-									{format(new Date(event.date), "MMM d")} -{" "}
-									{format(
-										new Date(
-											new Date(event.date).getTime() +
-												3 * 24 * 60 * 60 * 1000
-										),
-										"MMM d, yyyy"
-									)}
-								</CardDescription>
-							</div>
-							<div className="flex items-center space-x-2">
-								<Badge
-									className={`font-medium ${getGenderColor(event.gender)}`}
-									variant="outline"
-								>
-									{event.gender === "MEN"
-										? "Men's"
-										: "Women's"}
-								</Badge>
-								<Badge
-									className={`font-medium ${getWeaponColor(event.weapon)}`}
-									variant="outline"
-								>
-									{event.weapon.charAt(0) +
-										event.weapon.slice(1).toLowerCase()}
-								</Badge>
-								<Badge
-									className={`font-medium ${getTypeColor(event.type)}`}
-									variant="outline"
-								>
-									{event.type.charAt(0) +
-										event.type.slice(1).toLowerCase()}
-								</Badge>
-							</div>
-						</CardContent>
-					</Card>
-				))}
-			</div>
+									<div className="flex items-center space-x-2">
+										<Badge
+											className={`font-medium ${getGenderColor(event.gender)}`}
+											variant="outline"
+										>
+											{event.gender === "MEN"
+												? "Men's"
+												: "Women's"}
+										</Badge>
+										<Badge
+											className={`font-medium ${getWeaponColor(event.weapon)}`}
+											variant="outline"
+										>
+											{event.weapon.charAt(0) +
+												event.weapon
+													.slice(1)
+													.toLowerCase()}
+										</Badge>
+										<Badge
+											className={`font-medium ${getTypeColor(event.type)}`}
+											variant="outline"
+										>
+											{event.type.charAt(0) +
+												event.type
+													.slice(1)
+													.toLowerCase()}
+										</Badge>
+									</div>
+								</CardContent>
+							</Card>
+						))}
+					</div>
+				</div>
+			))}
 		</div>
-	));
+	);
 }
-
-/*
-											<Image
-												src={`https://flagcdn.com/w80/${event.countryCode.toLowerCase()}.png`}
-												alt={`${event.countryCode} flag`}
-												className="w-full h-full object-cover"
-												width={20}
-												height={20}
-												// onError={e => {
-												// 	(
-												// 		e.target as HTMLImageElement
-												// 	).src =
-												// 		"/placeholder.svg?height=24&width=32";
-												// }}
-											/>
-                                            */
