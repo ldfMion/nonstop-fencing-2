9688ee3909f2516a95699de0bbb0315f8b393ddd
ldfMion <lorenzodfmion@gmail.com>
2025-05-16 16:04:28 -0300

    added suspense to filter bc of hook

--- apps/web/src/app/competitions/(browse)/filter/index.tsx
+++ apps/web/src/app/competitions/(browse)/filter/index.tsx
@@ -1,15 +1,16 @@
+import { Suspense } from "react";
 import { FilterDrawer } from "./filter-drawer";
 import { SideFilter } from "./side-filter";
 
 export function Filter() {
 	return (
-		<>
+		<Suspense>
 			<div className="md:hidden">
 				<FilterDrawer />
 			</div>
 			<div className="hidden md:block">
 				<SideFilter />
 			</div>
-		</>
+		</Suspense>
 	);
 }
