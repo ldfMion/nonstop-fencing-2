c53048e1f9e3997adbdb9ad59cb27c71c4ba946e
ldfMion <lorenzodfmion@gmail.com>
2025-05-18 17:35:55 -0300

    added dynamicParams false to competition page

--- apps/web/src/app/competitions/[id]/page.tsx
+++ apps/web/src/app/competitions/[id]/page.tsx
@@ -14,6 +14,8 @@ import Link from "next/link";
 import { Fragment } from "react";
 import { router } from "~/lib/router";
 
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
 	const competitions = await QUERIES.getCompetitions(2025);
 	return competitions.map(c => c.id);
