95db9709014ce75fb8e0e1cbb56760a5a3074399
ldfMion <lorenzodfmion@gmail.com>
2025-05-19 09:16:48 -0300

    revalidated path for past brackets

--- apps/web/src/server/past-results.ts
+++ apps/web/src/server/past-results.ts
@@ -1,5 +1,7 @@
+import { router } from "~/lib/router";
 import { QUERIES } from "./db/queries";
 import { scrapePastEvent } from "./scraping/past-events";
+import { revalidatePath } from "next/cache";
 
 export async function updatePastResults(): Promise<void> {
 	const events = await QUERIES.getEventsWithMissingResults();
@@ -10,7 +12,10 @@ export async function updatePastResults(): Promise<void> {
 	console.log(
 		`Added ${events.length} to queue: ${events.map(e => e.id).join(", ")}.`
 	);
-	events.map(event => scrapePastEvent(event.id));
+	events.map(event => {
+		revalidatePath(router.event(event.id).bracket.past);
+		scrapePastEvent(event.id);
+	});
 	console.log(
 		`Updated ${events.length} events: ${events.map(e => e.id).join(", ")}.`
 	);
