b2c025b12667b458a7ded43d576b549244d083ba
ldfMion <lorenzodfmion@gmail.com>
2025-05-19 09:30:22 -0300

    fixed browserless bug

--- apps/web/src/server/past-results.ts
+++ apps/web/src/server/past-results.ts
@@ -2,6 +2,7 @@ import { router } from "~/lib/router";
 import { QUERIES } from "./db/queries";
 import { scrapePastEvent } from "./scraping/past-events";
 import { revalidatePath } from "next/cache";
+import { withBrowserless } from "./scraping/browserless";
 
 export async function updatePastResults(): Promise<void> {
 	const events = await QUERIES.getEventsWithMissingResults();
@@ -12,10 +13,16 @@ export async function updatePastResults(): Promise<void> {
 	console.log(
 		`Added ${events.length} to queue: ${events.map(e => e.id).join(", ")}.`
 	);
-	events.map(event => {
-		revalidatePath(router.event(event.id).bracket.past);
-		scrapePastEvent(event.id);
-	});
+	await withBrowserless([
+		async browser => {
+			await Promise.all(
+				events.map(async event => {
+					await scrapePastEvent(event.id, browser);
+					revalidatePath(router.event(event.id).bracket.past);
+				})
+			);
+		},
+	]);
 	console.log(
 		`Updated ${events.length} events: ${events.map(e => e.id).join(", ")}.`
 	);
