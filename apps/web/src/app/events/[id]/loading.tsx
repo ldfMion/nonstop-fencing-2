0db899a193a85afc7a683221acfc0d3455bec2fb
ldfMion <lorenzodfmion@gmail.com>
2025-05-20 10:52:02 -0300

    changed event overview to be ssg

--- apps/web/src/app/events/[id]/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function LoadingEventPage() {
-	return <p>Loading</p>;
-}
--- apps/web/src/app/events/[id]/page.tsx
+++ apps/web/src/app/events/[id]/page.tsx
@@ -1,5 +1,14 @@
 import { redirect } from "next/navigation";
 import { router } from "~/lib/router";
+import { QUERIES } from "~/server/db/queries";
+
+export const revalidate = false;
+
+export async function generateStaticParams() {
+	return (await QUERIES.getEventsWithResults()).map(e => ({
+		id: e.id.toString(),
+	}));
+}
 
 export default async function EventOverviewPage({
 	params,
