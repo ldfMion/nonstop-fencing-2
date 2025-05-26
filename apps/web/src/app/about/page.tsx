aa8ee51910761696266ca276ce761defcf8bc38e
ldfMion <lorenzodfmion@gmail.com>
2025-05-26 17:33:55 -0300

    removed unused imports

--- apps/web/src/app/about/page.tsx
+++ apps/web/src/app/about/page.tsx
@@ -1,8 +1,3 @@
-import Link from "next/link";
-import { Button } from "~/components/ui/button";
-import { ChevronRight } from "lucide-react";
-import { FeatureCards } from "./feature-cards";
-import { router } from "~/lib/router";
 import { Metadata } from "next";
 import { LandingPageSections } from "./sections";
 
